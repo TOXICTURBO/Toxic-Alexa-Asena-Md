c063aad6cabe3fc8b767cf371546475d01a6d0cd
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-09-16 16:13:56 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -1,89 +1,38 @@
-const { command } = require("../lib");
+const { command } = require('../lib/')
+command({pattern: 'pp ?(.*)', fromMe: true, desc: 'set profile picture in any resolution', type: 'user'}, async (message, match) => {
+if (!message.reply_message || !message.reply_message.image) return await message.send('_Reply to a image._')
+const media = await message.reply_message.downloadAndSaveMedia()
+await message.updateProfilePicture(message.user_id, media)
+await message.send('_Successfully Profile Picture Updated_')
+});
 
-command(
-  {
-    pattern: "setpp ?(.*)",
-    fromMe: true,
-    desc: "Set profile picture",
-    type: "user",
-  },
-  async (message, match, m) => {
-    if (!message.reply_message.image)
-      return await message.reply("_Reply to a photo_");
-    let buff = await m.quoted.download();
-    await message.setPP(message.user, buff);
-    return await message.reply("_Profile Picture Updated_");
-  }
-);
+command({pattern: 'fullpp ?(.*)', fromMe: true, desc: 'set profile picture in any resolution', type: 'user'}, async (message, match) => {
+if (!message.reply_message || !message.reply_message.image) return await message.send('_Reply to a image._')
+const media = await message.reply_message.downloadAndSaveMedia()
+await message.updateProfilePicture(message.user_id, media)
+await message.send('_Successfully Profile Picture Updated_')
+});
 
-command(
-  {
-    pattern: "setname ?(.*)",
-    fromMe: true,
-    desc: "Set User name",
-    type: "user",
-  },
-  async (message, match) => {
-    if (!match) return await message.reply("_Enter name_");
-    await message.updateName(match);
-    return await message.reply(`_Username Updated : ${match}_`);
-  }
-);
+command({pattern: 'gpp ?(.*)', fromMe: true, desc: 'set group icon in any resolution', type: 'group'}, async (message, match) => {
+if (!message.isGroup) return await message.send('_This command only works in group chats_')
+const isbotAdmin = await isBotAdmins(m)
+if (!isBotAdmins) return await message.send("I'm not an admin")
+if (!message.reply_message || !message.reply_message.image) return await message.send('_Reply to a image._')
+const media = await message.reply_message.downloadAndSaveMedia()
+await message.updateProfilePicture(message.jid, media)
+await message.send('_Successfully Group icon Updated_')
+});
 
-command(
-  {
-    pattern: "block ?(.*)",
-    fromMe: true,
-    desc: "Block a person",
-    type: "user",
-  },
-  async (message, match) => {
-    if (message.isGroup) {
-      let jid = message.mention[0] || message.reply_message.jid;
-      if (!jid) return await message.reply("_Reply to a person or mention_");
-      await message.block(jid);
-      return await message.send(`_@${jid.split("@")[0]} Blocked_`, {
-        mentions: [jid],
-      });
-    } else {
-      await message.block(message.jid);
-      return await message.reply("_User blocked_");
-    }
-  }
-);
+command({pattern: 'block', fromMe: true, desc: 'Block a person', type: 'user'}, async (message, match) => {
+const id = message.mention[0] || message.reply_message.sender || (!message.isGroup && message.jid)
+if (!id) return await message.send('*Give me a user*')
+await message.send('_Blocked_')
+await message.client.updateBlockStatus(id, 'block');
+});
 
-command(
-  {
-    pattern: "unblock ?(.*)",
-    fromMe: true,
-    desc: "Unblock a person",
-    type: "user",
-  },
-  async (message, match) => {
-    if (message.isGroup) {
-      let jid = message.mention[0] || message.reply_message.jid;
-      if (!jid) return await message.reply("_Reply to a person or mention_");
-      await message.block(jid);
-      return await message.send(`_@${jid.split("@")[0]} unblocked_`, {
-        mentions: [jid],
-      });
-    } else {
-      await message.unblock(message.jid);
-      return await message.reply("_User unblocked_");
-    }
-  }
-);
-
-command(
-  {
-    pattern: "jid",
-    fromMe: true,
-    desc: "Give jid of chat/user",
-    type: "user",
-  },
-  async (message, match) => {
-    return await message.send(
-      message.mention[0] || message.reply_message.jid || message.jid
-    );
-  }
-);
+command({pattern: 'unblock', fromMe: true, desc: 'Unblock a person', type: 'user'}, async (message, match) => {
+const id = message.mention[0] || message.reply_message.sender || (!message.isGroup && message.jid)
+if (!id) return await message.send('*Give me a user*')
+await message.send('_Unblocked_')
+await message.client.updateBlockStatus(id, 'unblock');
+});
