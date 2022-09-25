163a72bf3dde436eafd407d7407bc0b24ea7fa93
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-09-25 19:21:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,15 +20,15 @@ module.exports = {
   ANTILINK_ACTION: "kick",
   HANDLERS: process.env.HANDLERS || "^[,]",
   BRANCH: "master",
-  PACKNAME: process.env.PACKNAME || "X-asena",
+  PACKNAME: process.env.PACKNAME || "Toxic-Alexa_V4",
   WELCOME_MSG:
     process.env.WELCOME_MSG ||
     "{pp}Hi @user Welcome to @gname\nYou're our @count/513 Members ",
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-  AUTHOR: process.env.AUTHOR || "Team-Electra",
+  AUTHOR: process.env.AUTHOR || "TurboMods",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
-    DATABASE_URL === "./lib/database.db"
+    DATABASE_URL === "./lib/whatsasena.db"
       ? new Sequelize({
           dialect: "sqlite",
           storage: DATABASE_URL,
@@ -38,7 +38,7 @@ module.exports = {
           dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
           logging: false,
         }),
-  SUDO: process.env.SUDO || "918113921898,919544951258",
+  SUDO: process.env.SUDO || "916380260672,2347014889291",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
   WORK_TYPE : process.env.WORK_TYPE || "public"
